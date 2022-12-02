ce506dc98dec7d8dde6833577ea34fb7b158831c
OdairDias <odair2d@hotmail.com>
2022-12-02 14:24:38 -0300

    Fonte dados
    Signed-off-by: OdairDias <odair2d@hotmail.com>

---
+++ Pasta_01/Bibliotecas_fonte_dados.py
@@ -0,0 +1,12 @@
+#Fonte dos dados:http://www.ssp.sp.gov.br/transparenciassp/Consulta.aspx
+
+#Bibliotecas utilizadas: 
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+# Locale: Utilizado para formatação de datas, valores, dias de acordo com a região que queremos.
+import locale 
+#Warnings: Possui detalhes sobre os avisos e alertas que aparecem, porém podemos utiliza-lo também para que os alertas de
+#futuras atualizações e metodos depreciados não sejam exibidos
+import warnings
+warnings.filterwarnings("ignore") 
